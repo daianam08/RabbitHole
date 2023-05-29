1756be2e83c0f56a4622f83f05f063be0638b79d
DaianaM <daiana.m.0708@gmail.com>
2023-05-28 23:37:27 -0400

    Initial Commit

---
+++ RabbitHole/RabbitHoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RabbitHoleApp.swift
+//  RabbitHole
+//
+//  Created by scholar on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RabbitHoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
